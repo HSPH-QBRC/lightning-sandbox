0296b39ff11ff97d58ed051fa5ab14bdf7f3f29e
Brian Lawney <brianlawney@hsph.harvard.edu>
2023-04-12 09:11:16 -0400

    Adding data augmentations to cifar10

--- src/data_modules/cifar10.py
+++ src/data_modules/cifar10.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from hydra.utils import get_original_cwd
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader, random_split
+from torch import Generator
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
 
@@ -23,8 +24,35 @@ class CIFAR10DataModule(LightningDataModule):
             Path(dataset_cfg.base_dir) / Path(CIFAR10DataModule.NAME)
         self.batch_size = dataset_cfg.batch_size
         self.num_workers = dataset_cfg.num_workers
-        self.transformations = transforms.Compose([
-            transforms.ToTensor()
+        self.seed = dataset_cfg.seed
+        self._set_transforms()
+
+    def _set_transforms(self):
+        '''
+        Apply transforms for training, validation, and testing
+        '''
+        # taken from https://github.com/Lightning-Universe/lightning-bolts/\
+        # blob/master/pl_bolts/transforms/dataset_normalizations.py
+        normalize = transforms.Normalize(
+            mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+            std=[x / 255.0 for x in [63.0, 62.1, 66.7]],
+        )
+
+        self._train_transforms = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize
+        ])
+
+        self._validation_transforms = transforms.Compose([
+            transforms.ToTensor(),
+            normalize
+        ])
+
+        self._test_transforms = transforms.Compose([
+            transforms.ToTensor(),
+            normalize
         ])
 
     def prepare_data(self) -> None:
@@ -38,24 +66,38 @@ class CIFAR10DataModule(LightningDataModule):
         CIFAR10(self.data_dir, download=True, train=False)
 
     def setup(self, stage: str) -> None:
-        if stage == 'fit':
-            full_dataset = CIFAR10(self.data_dir, train=True,
-                                   transform=self.transformations)
-            self.train_dataset, self.val_dataset = random_split(
-                full_dataset, [0.8, 0.2])
+        if stage == 'fit' or stage is None:
+            # we first get the same datasets for train and validation, with
+            # the only difference being the applied transforms.
+            full_train_dataset = CIFAR10(self.data_dir, train=True,
+                                         transform=self._train_transforms)
+            full_validation_dataset = CIFAR10(self.data_dir, train=True,
+                                              transform=self._validation_transforms)
+
+            # we next split both "full" datasets to get our final train
+            # and validation sets. Note that we use the same generator for
+            # both so that we are not leaking training data into the
+            # validation set
+            self.train_dataset, _ = random_split(
+                full_train_dataset, [0.8, 0.2],
+                generator=Generator().manual_seed(self.seed))
+            _, self.val_dataset = random_split(
+                full_validation_dataset, [0.8, 0.2],
+                generator=Generator().manual_seed(self.seed))
 
         if stage == 'test':
             self.test_dataset = CIFAR10(self.data_dir, train=False,
-                                        transform=self.transformations)
+                                        transform=self._test_transforms)
 
         if stage == 'predict':
             self.test_dataset = CIFAR10(self.data_dir, train=False,
-                                        transform=self.transformations)
+                                        transform=self._test_transforms)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset,
                           batch_size=self.batch_size,
-                          num_workers=self.num_workers)
+                          num_workers=self.num_workers,
+                          shuffle=True)
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset,
