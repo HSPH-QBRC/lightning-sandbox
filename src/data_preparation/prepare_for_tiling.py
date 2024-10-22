3662d80dfe57dd438f680c9261d3af8a06c41027
Brian Lawney <brianlawney@hsph.harvard.edu>
2024-10-22 09:10:46 -0400

    attempt a different way to tile- more hierarchy to the subdirs

--- src/data_preparation/prepare_for_tiling.py
+++ src/data_preparation/prepare_for_tiling.py
@@ -3,6 +3,7 @@
 # for a sharded creation of input tile images.
 
 import argparse
+from logging import root
 from pathlib import Path
 
 import pandas as pd
@@ -10,6 +11,14 @@ import numpy as np
 
 # the maximum number of files permitted in a single directory
 N_FILES_MAX = 9999
+
+# we plan to create a tree of directories. Each leaf 
+# directory will contain the num_tiles number of images.
+# N_DIRS controls (roughly) how many directories there are 
+# at each level above that. For example, if we have 10,000
+# full-size images, we can have a top level of 100 folders
+# each of while will contain 100 folders. 
+TARGET_N_DIRS = 100
 
 
 def parse_args():
@@ -27,7 +36,27 @@ def parse_args():
     parser.add_argument('-l', '--level', type=int, default=0)
     parser.add_argument('-r', '--resize', type=int, default=None)
     parser.add_argument('-m', '--mode', type=int, default=0)
+    parser.add_argument('-k', '--num_shards', type=int, default=50)
     return parser.parse_args()
+
+
+def construct_hierarchy(root_dir, n_per_level, total_levels, current_level, total_dirs):
+    dirs = []
+    if current_level < total_levels:
+        for i in range(n_per_level):
+            dirs.extend(
+                construct_hierarchy(root_dir / str(i),
+                                    n_per_level,
+                                    total_levels,
+                                    current_level + 1,
+                                    total_dirs)
+            )
+    else:
+        for i in range(n_per_level):
+            d = root_dir / str(i)
+            d.mkdir(parents=True)
+            dirs.append(d)
+    return dirs
 
 
 def main():
@@ -37,6 +66,7 @@ def main():
     tile_size = args.tile_size
     level = args.level
     mode = args.mode
+    num_shards = args.num_shards
 
     # has image_id, data_provider, isup_grade, gleason_score
     train_metadata = pd.read_csv(args.train_metadata_file)
@@ -49,29 +79,30 @@ def main():
     n_fullsize_train_imgs = train_metadata.shape[0]
 
     # to prevent us from crippling the filesystem with tons of files, 
-    # we need to create subdirectories inside `output_dir`. Given that
-    # each full-size image will generate `n_tiles`, create numbered subdirs
-    # accordingly.
-    
-    # we want all the tiles corresponding to a single full-size image
-    # to be in the same subdir. This dictates how many full-size images
-    # we can assign to a particular subdir based on the max number of files
-    # which can be stored in any one subdir:
-    fullsize_img_per_subdir = N_FILES_MAX // n_tiles
+    # we need to create subdirectories inside `output_dir`.
+    # Each full-size image will generate `n_tiles` which we place in the same
+    # directory. Above that, we create a hierarchy such that we have a 'reasonable'
+    # number of folders at each level. As an example, if we had 50k full-size
+    # images, then dir_levels would be 2.0 and n_dirs = 224. Thus, at the top-level
+    # of the hierachy, we would have 224 folders. At the second level, we would place
+    # 224 directories in each of the parents until we reach the desired 50k directories.
+    dir_levels = np.round(np.log(n_fullsize_train_imgs)/np.log(TARGET_N_DIRS))
+    dirs_per_level = np.ceil(np.power(n_fullsize_train_imgs, 1/dir_levels))
 
-    num_subdirs = 1 + (n_fullsize_train_imgs // fullsize_img_per_subdir)
+    # pre-create the subdirs- not that this will create extra empty dirs if the
+    # number of full-size images is not equal to dirs_per_level^dir_levels. Hence the truncation
+    image_subdirs = construct_hierarchy(output_dir, dirs_per_level, dir_levels, 1, n_fullsize_train_imgs)[:n_fullsize_train_imgs]
 
-    # pre-create the subdirs
-    for i in range(num_subdirs):
-        (output_dir / str(i)).mkdir()
-
-    # to help with the sharding operation, we will specify which images will go to 
-    # which subdirectories.
-    image_subdirs = np.repeat(np.arange(num_subdirs), fullsize_img_per_subdir)[:n_fullsize_train_imgs]
+    # Specify which images will go to which subdirectories.
     train_metadata['image_subdir'] = image_subdirs
 
+    # to aid with sharding, we assign the data to one of the K shards:
+    num_per_shard = (n_fullsize_train_imgs // num_shards) + 1
+    shard_assignments = np.repeat(np.arange(num_shards), num_per_shard)[:n_fullsize_train_imgs]
+    train_metadata['shard'] = shard_assignments
+
     # write this file so we can shard the tile creation process.
-    train_metadata.to_csv(f'{args.train_metadata_file.parent}/training_metadata_with_shards.csv', index=False)
+    train_metadata.to_csv(f'{args.train_metadata_file.parent}/training_metadata_with_shards_and_subdirs.csv', index=False)
 
 
 if __name__ == '__main__':
